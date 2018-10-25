07ef3c89792a1c7c6cf54ad728e92f37b74c09d6
Gabriel de Oliveira Koehler <32876476+Mitysonly@users.noreply.github.com>
2018-10-25 02:04:32 -0300

    Gabriel de O. Koehler - Repositório inicial Projeto AED-III

---
+++ projetoSistema/src/projetoSistema.java
@@ -0,0 +1,17 @@
+
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class projetoSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          
+    new Telas.telaLogin().show();    
+        
+      
+    
+    }   
+}
